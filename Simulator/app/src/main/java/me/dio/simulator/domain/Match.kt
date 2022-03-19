421ff3671ce206f7a4c9478fe04c60795ab8229c
Izabella Terêncio <izahterencio@gmail.com>
2022-03-18 22:14:39 -0300

    Utilizando Retrofit para consumir a API da aplicação

--- Simulator/app/src/main/java/me/dio/simulator/domain/Match.kt
+++ Simulator/app/src/main/java/me/dio/simulator/domain/Match.kt
@@ -1,10 +1,16 @@
 package me.dio.simulator.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Match(
 
+    @SerializedName("descricao")
     val description: String,
+    @SerializedName("local")
     val place: Place,
+    @SerializedName("mandante")
     val home: Team,
+    @SerializedName("visitante")
     val visitor: Team
 
 )
--- Simulator/app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ Simulator/app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -5,11 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import me.dio.simulator.R;
+import me.dio.simulator.data.MatchesAPI;
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.domain.Match;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity{
 
     private ActivityMainBinding binding;
+    private MatchesAPI matchesApi;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -18,10 +31,40 @@ public class MainActivity extends AppCompatActivity{
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setupHttpClient();
+        setupMatchesList();
+        setupMatchesRefresh();
+        setupFloatingActionButton();
+
+    }
+
+    // Neste método são criadas todas as estruturas necessárias ao Retrofit
+    private void setupHttpClient(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://izahterencio.github.io/matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        matchesApi = retrofit.create(MatchesAPI.class);
     }
 
     private void setupMatchesList(){
-        // TODO: Listar partidas
+        matchesApi.getMatches()
+                .enqueue(new Callback<List<Match>>() {
+                    @Override
+                    public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                        if(response.isSuccessful()){
+                            List<Match> matches = response.body();
+                        } else{
+                            showErrorMessage();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Match>> call, Throwable t) {
+                        showErrorMessage();
+                    }
+                });
     }
 
     private void setupMatchesRefresh(){
@@ -30,6 +73,10 @@ public class MainActivity extends AppCompatActivity{
 
     private void setupFloatingActionButton(){
         // TODO: Criar evento de clique
+    }
+
+    private void showErrorMessage(){
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
 
 }
