4dc0e6f9f93e1272ab55cd8cb2f4b7140acc9a9a
Izabella Terêncio <izahterencio@gmail.com>
2022-03-18 23:13:51 -0300

    Implementando Adapter para o RecyclerView

--- Simulator/app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ Simulator/app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -13,6 +15,7 @@ import me.dio.simulator.R;
 import me.dio.simulator.data.MatchesAPI;
 import me.dio.simulator.databinding.ActivityMainBinding;
 import me.dio.simulator.domain.Match;
+import me.dio.simulator.ui.adapter.MatchesAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity{
 
     private ActivityMainBinding binding;
     private MatchesAPI matchesApi;
+    private RecyclerView.Adapter matchesAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -49,12 +53,17 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void setupMatchesList(){
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+
         matchesApi.getMatches()
                 .enqueue(new Callback<List<Match>>() {
                     @Override
                     public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
                         if(response.isSuccessful()){
                             List<Match> matches = response.body();
+                            matchesAdapter = new MatchesAdapter(matches);
+                            binding.rvMatches.setAdapter(matchesAdapter);
                         } else{
                             showErrorMessage();
                         }
